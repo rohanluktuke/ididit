afc842b115d78c562ca1fb942ad3219109a92fa5
Rohan Luktuke <rohan.luktuke@capitalone.com>
2018-01-07 12:32:41 -0500

    basic working setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { AccomplishmentService } from './accomplishment.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AccomplishmentService]
 })
 export class AppComponent {
   title = 'app';
+  loadedFeature = 'create';
+
+  onNavigate(selection: string) {
+    this.loadedFeature = selection;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { EnterAccomplishmentsComponent } from './enter-accomplishments/enter-accomplishments.component';
+import { ViewAccomplishmentsComponent } from './view-accomplishments/view-accomplishments.component';
+import { SigninComponent } from './signin/signin.component';
+import { Routes, RouterModule } from '@angular/router';
+import { ViewOneAccomplishmentComponent } from './view-accomplishments/view-one-accomplishment/view-one-accomplishment.component';
+
+const appRoutes: Routes = [
+  { path: '', component: EnterAccomplishmentsComponent},
+  { path: 'create', component: EnterAccomplishmentsComponent},
+  { path: 'view', component: ViewAccomplishmentsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EnterAccomplishmentsComponent,
+    ViewAccomplishmentsComponent,
+    SigninComponent,
+    ViewOneAccomplishmentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
