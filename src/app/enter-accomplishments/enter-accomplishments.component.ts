a93581bc8dbf280f9b7eeac0bfc4c0367cb35549
Rohan Luktuke <rohanluktuke@gmail.com>
2018-01-25 22:03:01 -0500

    updated questions

--- src/app/enter-accomplishments/enter-accomplishments.component.ts
+++ src/app/enter-accomplishments/enter-accomplishments.component.ts
@@ -13,7 +13,9 @@ export class EnterAccomplishmentsComponent implements OnInit {
   @ViewChild('aTypeInput') aTypeInputRef: ElementRef;
   @ViewChild('aWhatInput') aWhatInputRef: ElementRef;
   @ViewChild('aHowInput') aHowInputRef: ElementRef;
-  @ViewChild('aElseInput') aElseInputRef: ElementRef;
+  @ViewChild('aResultInput') aResultInputRef: ElementRef;
+  @ViewChild('aExtraCurricularInput') aExtraCurricularInputRef: ElementRef;
+  @ViewChild('aNextGoalsInput') aNextGoalsInputRef: ElementRef;
   constructor(private accomplishmentService: AccomplishmentService,
               private authService: AuthService) { }
 
@@ -25,14 +27,18 @@ export class EnterAccomplishmentsComponent implements OnInit {
     const typeinput = this.aTypeInputRef.nativeElement.value;
     const whatinput = this.aWhatInputRef.nativeElement.value;
     const howinput = this.aHowInputRef.nativeElement.value;
-    const elseinput = this.aElseInputRef.nativeElement.value;
+    const resultinput = this.aResultInputRef.nativeElement.value;
+    const extracurricularinput = this.aExtraCurricularInputRef.nativeElement.value;
+    const nextgoalsinput = this.aNextGoalsInputRef.nativeElement.value;
     console.log(typeinput);
     const uid = this.authService.getCurrentUserUUID();
     const accomplishment = new Accomplishment(
       typeinput,
       whatinput,
       howinput,
-      elseinput,
+      resultinput,
+      extracurricularinput,
+      nextgoalsinput,
       uid,
       '123',
       new Date(),
